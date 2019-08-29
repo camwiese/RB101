01c107644723f6e64e9070995a3b466a4887ccc0
Cam Wiese <camwiese@gmail.com>
2019-08-28 21:30:08 -0400

    Calculator exercise

---
+++ lesson_2/calculator.rb
@@ -0,0 +1,23 @@
+## calculator
+
+Kernel.puts("Welcome to the calculator")
+Kernel.puts("What's the first number?")
+number1 = Kernel.gets().chomp()
+
+Kernel.puts("What's the second number?")
+number2 = Kernel.gets().chomp()
+
+Kernel.puts("What operation would you like to perform? 1) add 2) subtract 3) miltiply 4) divide")
+operator = Kernel.gets().chomp()
+
+if operator == '1'
+  result = number1.to_i + number2.to_i
+elsif operator == '2'
+  result = number1.to_i() - number2.to_i()
+elsif operator == '3'
+  result = number1.to_i * number2.to_i()
+else
+  result = number1.to_f() / number2.to_f()
+end
+
+Kernel.puts("The result is #{result}")
