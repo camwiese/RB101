9873dcf7e2c939b38ab47e3cd4e808a198426932
Cam Wiese <camwiese@gmail.com>
2019-09-18 18:24:07 -0700

    Another test file for Module 4

---
+++ lesson_4/test2.rb
@@ -0,0 +1,11 @@
+countries_and_capitals = {
+  'France' => 'Paris',
+  'Belgium' => 'Brussels',
+  'Morocco' => 'Rabat',
+  'Barbados' => 'Bridgetown',
+  'Peru' => 'Lima',
+  'Bolivia' => 'La Paz',
+  'Brazil' => 'Brasilia'
+}
+
+p countries_and_capitals["Bolivia"].chr
